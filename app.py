cfa0b72f62df40286ded6a41803bfab86844be6a
LeeVaughn <dlvprogramming@gmail.com>
2021-12-18 09:23:11 -0800

    Create initial routes

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello!'
+
+
+@app.route('/project/new')
+def new_project():
+    return 'Hello!'
+
+
+@app.route('/project/{id}')
+def project():
+    return 'Hello!'
+
+
+@app.route('/project/{id}/edit')
+def edit_project():
+    return 'Hello!'
+
+
+@app.route('/project/{id}/delete')
+def delete_project():
+    return 'Hello!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.0.1')
